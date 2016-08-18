7e84ac78ba007bb00a251dabc213f174facd41ed
Caleb Faught <Kahleeb@Calebs-MBP.fios-router.home>
2016-08-18 10:17:17 -0500

    added post method to signup and create a new user upon submitting form

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,4 +4,15 @@ class UserController < ApplicationController
     erb :'/users/signup'
   end
 
+  post '/signup' do
+    binding.pry
+    @user = User.create(params[:user])
+    redirect "/users/#{@user.id}"
+  end
+
+  get '/users/:id' do
+    @user = User.find(params[:id])
+    erb :'/users/show'
+  end
+
 end
