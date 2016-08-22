c0a21a4315a9754a2cff37068d823b39952f0f62
Caleb Faught <Kahleeb@Calebs-MacBook-Pro.local>
2016-08-22 12:47:17 -0500

    added a delete function for fish

--- app/controllers/creature_controller.rb
+++ app/controllers/creature_controller.rb
@@ -5,6 +5,28 @@ class CreatureController < ApplicationController
     erb :'/creatures/new'
   end
 
+  get '/fish/:id/edit' do
+    @creature = Creature.find_by(id: params[:id])
+    @log_entry = @creature.log_entry
+    erb :'/creatures/edit'
+  end
+
+  get '/fish/:id/delete' do
+    if logged_in?
+      creature = Creature.find_by(id: params[:id])
+      log_entry = creature.log_entry
+      # only the user who created a fish can delete it
+      if current_user.id == creature.log_entry.logbook.user_id
+        creature.destroy
+        redirect "/log_entry/#{log_entry.id}"
+      else
+        redirect "/fish/#{creature.id}"
+      end
+    else
+      redirect '/login'
+    end
+  end
+
   get '/fish' do
     erb :'/creature/index'
   end
@@ -24,10 +46,21 @@ class CreatureController < ApplicationController
 
   end
 
+  post '/fish/:id' do
+    if logged_in?
+      @creature = Creature.find_by(id: params[:id])
+      @creature.update(params[:fish])
+      @log_entry = @creature.log_entry
+      erb :'/creatures/show'
+    else
+      redirect '/login'
+    end
+  end
+
   get '/fish/:id' do
     if logged_in?
       @creature = Creature.find_by(id: params[:id])
-      @log_entry = @creature.log_entry
+      @log_entry = LogEntry.find_by(id: @creature.log_entry_id)
       erb :'/creatures/show'
     else
       redirect '/login'
