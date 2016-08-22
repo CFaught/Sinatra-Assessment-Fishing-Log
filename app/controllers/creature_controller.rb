08ffbef0fb475f903eb94c3b8a93b1313100e760
Caleb Faught <Kahleeb@Calebs-MacBook-Pro.local>
2016-08-22 12:03:45 -0500

    added an add catch for log entry

--- app/controllers/creature_controller.rb
+++ app/controllers/creature_controller.rb
@@ -1,7 +1,27 @@
 class CreatureController < ApplicationController
 
+  get '/log_entry/:id/add_fish' do
+    @log_entry = LogEntry.find_by(id: params[:id])
+    erb :'/creatures/new'
+  end
+
   get '/fish' do
-    erb :'/creatures/index'
+    erb :'/creature/index'
+  end
+
+  post '/fish' do
+    binding.pry
+    if logged_in?
+      fish = Creature.new(params[:fish])
+      if fish.save
+        redirect "/fish/#{ fish.id }"
+      else
+        redirect "/log_entry/#{ params[:fish][:log_entry_id] }/add_fish"
+      end
+    else
+      redirect '/login'
+    end
+
   end
 
   get '/fish/:id' do
