c212d39cd3c935d4a28dcee021539f0ed8000b08
Caleb Faught <Kahleeb@Calebs-MacBook-Pro.local>
2016-08-22 11:20:12 -0500

    Added log_entry edit and debugged a datetime formatting issue

--- app/controllers/log_entry_controller.rb
+++ app/controllers/log_entry_controller.rb
@@ -11,9 +11,12 @@ class LogEntryController < ApplicationController
   end
 
   post '/log_entry' do
-    binding.pry
     if logged_in?
       log_entry = LogEntry.new(params[:log_entry])
+
+      log_entry.date = params[:date].to_s + " " + params[:time].to_s
+      log_entry.date.to_datetime
+
       logbook = current_user.logbooks.find_by(id: params[:logbook][:id])
 
       if logbook
@@ -31,11 +34,47 @@ class LogEntryController < ApplicationController
     end
   end
 
+  post '/log_entry/:id' do
+    if logged_in?
+      log_entry = current_user.log_entries.find_by(id: params[:id])
+      logbook = current_user.logbooks.find_by(id: params[:logbook][:id])
+
+      if logbook
+        log_entry.update(params[:log_entry])
+
+        # make sure to save the date and time correctly with timezone
+        zone = log_entry.date.to_time
+        log_entry.date = params[:date].to_s + " " + params[:time].to_s + ":00 " + zone.to_s
+        log_entry.date.to_datetime
+
+        log_entry.logbook_id = logbook.id
+
+        if log_entry.save
+          redirect "/log_entry/#{ log_entry.id }"
+        else
+          redirect "/log_entry/#{ log_entry.id }/edit"
+        end
+      else
+        redirect "/log_entry/#{ log_entry.id }/edit"
+      end
+    else
+      redirect '/login'
+    end
+  end
+
   get '/log_entry/:id' do
     @log_entry = current_user.log_entries.find_by(id: params[:id])
     @logbook = Logbook.find_by(id: @log_entry.logbook_id)
     @fish = @log_entry.creatures
     erb :'/log_entries/show'
+  end
+
+  get '/log_entry/:id/edit' do
+    @log_entry = current_user.log_entries.find_by(id: params[:id])
+    @logbooks = current_user.logbooks
+    @date = @log_entry.date.to_date
+    @time = @log_entry.date.to_time
+    erb :'/log_entries/edit'
   end
 
   get '/log_entry/:id/delete' do
